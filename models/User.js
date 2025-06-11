62b47f52ff070d3232e7dfdb6f83bc17d35630f9
Mohammed AbuShallouf <109415965+Mohd2040@users.noreply.github.com>
2025-06-11 18:49:46 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// ✅ تشفير كلمة السر قبل الحفظ
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
